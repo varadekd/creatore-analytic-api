88fa977f2a3c8ae94def6bf84225f78c35cd960d
kush.varade.19 <kush.varade.19@gmail.com>
2023-10-22 22:16:56 +0530

    Variable renaming for sales controller

--- app/models/sales.rb
+++ app/models/sales.rb
@@ -3,13 +3,13 @@ require 'json'
 class Sales 
     def self.all
         begin
-            salesDataFile = File.read(Rails.root.join('db' , 'data' , 'sales.json'))
-            salesData = JSON.parse(salesDataFile)
+            sales_data_file = File.read(Rails.root.join('db' , 'data' , 'sales.json'))
+            sales_data = JSON.parse(sales_data_file)
         rescue Errno::ENOENT
             { error: 'Sales data file not found' }
         rescue JSON::ParserError
             { error: 'Error is parsing json data for sales' }
         end
-        salesData
+        sales_data
     end
 end
